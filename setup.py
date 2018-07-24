74fb5018316537c7d493ab42f3b173a6f92a3fbc
Bulak Arpat <Bulak.Arpat@unil.ch>
2018-07-24 16:58:49 +0200

    post-install

--- setup.py
+++ setup.py
@@ -9,41 +9,32 @@ from setuptools.command.develop import develop
 from setuptools.command.install import install
 
 
-class PostDevelopCommand(develop):
-    """Post-installation for development mode."""
-    def run(self):
-        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-        print("Do you see me?")
-        develop.run(self)
 
+def _post_install(my_setup):
+    def _post_actions():
+        print("THIS WORKED")
+    _post_actions()
+    return my_setup
 
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-        install.run(self)
-
-setup(
-    name='pipeline',
-    version='0.1.0',
-    description='Tools for analysing RPF-Seq data',
-#   url='NA',
-    author='Bulak Arpat',
-    author_email='bulak.arpat@gmail.com',
-    license='GPLv3',
-    packages=find_packages(),
-    scripts=['scripts/meta_pipeline.sh',
-             'scripts/pipeline_bwt2_single.sh',
-             'scripts/pipeline_common.sh',
-             'scripts/subpipeline.sh'],
-    entry_points={
-        'console_scripts': ['make_sample_db = pipeline.make_sample_db:main',
-                            'concat_map_logs = pipeline.concat_map_logs:main']},
-    install_requires=[],
-    dependency_links=[],
-    cmdclass={
-        'develop': PostDevelopCommand,
-        'install': PostInstallCommand,
-    },
-    zip_safe=False
+_post_install(
+    setup(
+        name='pipeline',
+        version='0.1.0',
+        description='Tools for analysing RPF-Seq data',
+    #   url='NA',
+        author='Bulak Arpat',
+        author_email='bulak.arpat@gmail.com',
+        license='GPLv3',
+        packages=find_packages(),
+        scripts=['scripts/meta_pipeline.sh',
+                 'scripts/pipeline_bwt2_single.sh',
+                 'scripts/pipeline_common.sh',
+                 'scripts/subpipeline.sh'],
+        entry_points={
+            'console_scripts': ['make_sample_db = pipeline.make_sample_db:main',
+                                'concat_map_logs = pipeline.concat_map_logs:main']},
+        install_requires=[],
+        dependency_links=[],
+        zip_safe=False
+    )
 )
